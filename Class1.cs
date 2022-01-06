6d8044d7627ffe6c15b6f38ea4cd0071646410c0
Ingole987 <pratik.ingole98@gmail.com>
2022-01-06 11:13:29 +0530

    added:GenericMethod

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericMethod;
+namespace GenericMethod
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 2, 4, 5, 6, 8, 7 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+            double[] doubleArray = { 1.1, 3.3, 2.2, 4.4 };
+            //DisplayInt(intArray);
+            //DisplayChar(charArray);
+            //DisplayDouble(doubleArray);
+            Display<int>(intArray);
+            Display<char>(charArray);
+            Display<double>(doubleArray);
+            Console.ReadKey();
+        }
+        public static void Display<T>(T[] test)
+        {
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.Write(test[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
